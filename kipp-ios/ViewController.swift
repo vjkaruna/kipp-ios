bd885fbef91d3fb87e2abb3dcd13817c9e2ab279
vjkaruna <vjkaruna@gmail.com>
2014-10-09 17:21:43 -0700

    Initial Commit

---
+++ kipp-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kipp-ios
+//
+//  Created by Vijay Karunamurthy on 10/9/14.
+//  Copyright (c) 2014 vjkaruna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
