084dbb3224a3e451d879aa0faec30a8f76398fff
Vanessa <vanessa.li@rd.io>
2014-10-18 14:57:10 -0700

    refactor Parse call out of Student model

--- kipp-ios/Student.swift
+++ kipp-ios/Student.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 
-//let studentAttendanceStateChanged = "studentAttendanceStateChanged"
-
 enum AttendanceType: Int {
     case Absent = 1, Tardy, Present
 }
@@ -20,14 +18,7 @@ class Student: NSObject {
     
     var delegate: StudentProfileChangedDelegate?
     
-    var attendance: AttendanceType? /*{
-        willSet {
-            if newValue != nil && newValue != .Present {
-                // Send notification only for absent or tardy updates
-                self.delegate?.attendanceDidChange()
-            }
-        }
-    }*/
+    var attendance: AttendanceType?
     
     init(obj: PFObject) {
         self.firstName = obj["firstName"] as NSString
@@ -46,33 +37,17 @@ class Student: NSObject {
     }
     
     func markAttendanceForType(type: AttendanceType, forDate: NSDate = NSDate.date()) {
-        var absentEntry = PFObject(className: "Attendance")
-        absentEntry["type"] = type.toRaw()
-        absentEntry["date"] = forDate.beginningOfDay()
-        absentEntry["studentId"] = self.studentId
-        absentEntry.saveInBackground() // or save eventually?
+        ParseClient.sharedInstance.markAttendance(self.studentId, attendance: type, forDate: forDate)
         self.attendance = type
         self.delegate?.attendanceDidChange()
     }
     
     func fillAttendanceState(forDate: NSDate = NSDate.date()) {
         if attendance == nil {
-            var isAbsentQ = PFQuery(className: "Attendance")
-            isAbsentQ.whereKey("studentId", equalTo: self.studentId)
-//            isAbsentQ.whereKey("type", equalTo: AttendanceType.Absent.toRaw())
-            isAbsentQ.whereKey("date", equalTo: forDate.beginningOfDay())
-            isAbsentQ.findObjectsInBackgroundWithBlock({ (results, error) -> Void in
-                if error == nil {
-                    if results.count > 0 {
-                        let result = results[0] as PFObject
-                        let rawAttendanceType = result["type"] as Int
-                        self.attendance = AttendanceType.fromRaw(rawAttendanceType)
-                        self.delegate?.attendanceDidChange()
-                        NSLog("Found entry for student \(self.firstName)")
-                    } else {
-                        self.attendance = .Present
-                        NSLog("\(self.firstName) present")
-                    }
+            ParseClient.sharedInstance.getAttendanceForDateWithCompletion(self.studentId, forDate: forDate, completion: { (attendanceType, error) -> () in
+                if attendanceType != nil {
+                    self.attendance = attendanceType
+                    self.delegate?.attendanceDidChange()
                 } else {
                     NSLog("error: \(error)")
                 }
@@ -80,30 +55,6 @@ class Student: NSObject {
         } else {
             NSLog("Attendance state for \(self.firstName) is \(self.attendance!.toRaw())")
         }
-    }
-}
-
-let calendar = NSCalendar(identifier: NSGregorianCalendar)
-
-extension NSDate {
-    func isSameDay(date: NSDate) -> Bool {
-        let dateComponents = calendar.components( .DayCalendarUnit | .MonthCalendarUnit | .YearCalendarUnit, fromDate: date)
-        let nowComponents = calendar.components( .DayCalendarUnit | .MonthCalendarUnit | .YearCalendarUnit, fromDate: NSDate())
-        return (dateComponents.day == nowComponents.day && dateComponents.month == nowComponents.month && dateComponents.year == nowComponents.year)
-    }
-    func beginningOfDay() -> NSDate {
-        let dateComponents = calendar.components( .DayCalendarUnit | .MonthCalendarUnit | .YearCalendarUnit, fromDate: self)
-        dateComponents.hour = 0
-        dateComponents.minute = 0
-        dateComponents.second = 0
-        return calendar.dateFromComponents(dateComponents)!
-    }
-    func endOfDay() -> NSDate {
-        let dateComponents = calendar.components( .DayCalendarUnit | .MonthCalendarUnit | .YearCalendarUnit, fromDate: self)
-        dateComponents.hour = 23
-        dateComponents.minute = 59
-        dateComponents.second = 59
-        return calendar.dateFromComponents(dateComponents)!
     }
 }
 
