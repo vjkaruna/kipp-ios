5e290bb7b55d4b57a1014461f4910d96dd2627cf
Vanessa <vanessa.li@rd.io>
2014-10-23 01:31:28 -0700

    Hook action buttons to Parse

---
+++ kipp-ios/Action.swift
@@ -0,0 +1,13 @@
+//
+//  Action.swift
+//  kipp-ios
+//
+//  Created by vli on 10/23/14.
+//  Copyright (c) 2014 vjkaruna. All rights reserved.
+//
+
+import UIKit
+
+class Action: NSObject {
+   
+}
