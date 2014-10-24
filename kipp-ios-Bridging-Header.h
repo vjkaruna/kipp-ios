fe9da3228ea1b5e8b0a9f850fa3f603b4bea5f78
vjkaruna <vjkaruna@gmail.com>
2014-10-23 19:03:40 -0700

    Graph import fix.

--- kipp-ios-Bridging-Header.h
+++ kipp-ios-Bridging-Header.h
@@ -2,4 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
- #import <Parse/Parse.h>
+#import <Parse/Parse.h>
+#import "Pods/GraphKit/Source/LineGraph/GKLineGraph.h"
+#import "Pods/GraphKit/Source/UIColor+GraphKit.h"
---
+++ kipp-ios/ProfileGraphViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ProfileGraphViewController.swift
+//  kipp-ios
+//
+//  Created by Vijay Karunamurthy on 10/23/14.
+//  Copyright (c) 2014 vjkaruna. All rights reserved.
+//
+
+import UIKit
+
+class ProfileGraphViewController: UIViewController, GKLineGraphDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
