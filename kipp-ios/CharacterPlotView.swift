c4a5b3b56cfce0d226f8aa47b9aeff338b548c02
Vanessa <vanessa.li@rd.io>
2014-11-07 03:36:22 -0800

    paging graph views

---
+++ kipp-ios/CharacterPlotView.swift
@@ -0,0 +1,21 @@
+//
+//  CharacterPlotView.swift
+//  kipp-ios
+//
+//  Created by vli on 11/7/14.
+//  Copyright (c) 2014 vjkaruna. All rights reserved.
+//
+
+import UIKit
+
+class CharacterPlotView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
