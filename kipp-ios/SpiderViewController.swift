6206c1d0267dcfc61bc3b8cd39615094e6ed636c
Vanessa <vanessa.li@rd.io>
2014-11-07 01:10:53 -0800

    cleanup

--- kipp-ios/SpiderViewController.swift
+++ kipp-ios/SpiderViewController.swift
@@ -15,21 +15,24 @@ class SpiderViewController: UIViewController {
     var scoresLoaded: Bool = false
     var spiderView: BTSpiderPlotterView!
 
+    let characterTraits = CharacterTrait.defaultCharacterTraitArray()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        NSLog("Load spider view?")
+        
+        var dict = NSMutableDictionary()
+        for index in 0..<characterTraits.count {
+            dict.setValue("0", forKey: characterTraits[index].title)
+        }
 
-        var dict = ["A": "0", "B": "0", "C": "0", "D": "0", "E": "0", "F": "0", "G": "0"]
         spiderView = BTSpiderPlotterView(frame: self.view.frame, valueDictionary: dict)
-//        var values = ["A": "0", "B": "0", "C": "0"]
-//        spiderView = BTSpiderPlotterView(frame: self.view.frame, valueDictionary: values)
         spiderView.maxValue = 1.0
         spiderView.valueDivider = 0.1
         spiderView.drawboardColor = UIColor(red: CGFloat(96.0/255.0), green: CGFloat(162.0/255.0), blue: CGFloat(215.0/255.0), alpha: CGFloat(0.5))
         spiderView.plotColor = UIColor(red: CGFloat(96.0/255.0), green: CGFloat(162.0/255.0), blue: CGFloat(215.0/255.0), alpha: CGFloat(0.5))
         self.view.addSubview(spiderView)
         
-        loadedTraitValues = [Int](count: 7, repeatedValue: 0)
+        loadedTraitValues = [Int](count: characterTraits.count, repeatedValue: 0)
         loadScores()
     }
 
@@ -39,20 +42,18 @@ class SpiderViewController: UIViewController {
     }
     
     func loadScores() {
-//        for index in 0..<student.characterArray.count {
         let characterTraits = CharacterTrait.defaultCharacterTraitArray()
-        for index in 0..<7 {
+        for index in 0..<characterTraits.count {
             let curCharacterTrait = characterTraits[index]
             
             ParseClient.sharedInstance.getLatestCharacterScoreForWeekWithCompletion(studentId, characterTrait: curCharacterTrait.title) { (characterTrait, error) -> () in
                 if characterTrait != nil {
                     NSLog("Found \(characterTrait!.title) with score \(characterTrait!.score)")
-//                    self.student!.characterArray[index].score = characterTrait!.score
                     self.loadedTraitValues[index] = characterTrait!.score
                 } else {
                     self.loadedTraitValues[index] = 0
                 }
-                if index == 6 {
+                if index == characterTraits.count-1 {
                     self.scoresLoaded = true
                     self.drawGraph()
                 }
@@ -62,18 +63,12 @@ class SpiderViewController: UIViewController {
 
     func drawGraph() {
         var values = getNormalizedValues()
-        let characterTraits = CharacterTrait.defaultCharacterTraitArray()
+        
         var dict: NSMutableDictionary = NSMutableDictionary()
         for index in 0..<characterTraits.count {
             dict.setValue(values[index], forKey: characterTraits[index].title)
-//            var dict = ["A": "\(values[0])", "B": "\(values[1])", "C": "\(values[2])", "D": "\(values[3])", "E": "\(values[4])", "F": "\(values[5])", "G": "\(values[6])"]
         }
-//        var dict = ["A": "\(values[0])", "B": "\(values[1])", "C": "\(values[2])", "D": "\(values[3])", "E": "\(values[4])", "F": "\(values[5])", "G": "\(values[6])"]
         NSLog("\(dict)")
-//        spiderView = BTSpiderPlotterView(frame: self.view.frame, valueDictionary: dict)
-//        spiderView.maxValue = 1.0
-//        spiderView.valueDivider = 0.1
-//        self.view.addSubview(spiderView)
         
         spiderView.animateWithDuration(0.5, valueDictionary: dict)
     }
